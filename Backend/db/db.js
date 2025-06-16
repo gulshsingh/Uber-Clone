f5a5d639376717ce40bfa6cbf83578d97ba9faa4
Gulshanbaghel <singhgulshan0000@gmail.com>
2025-06-17 00:11:37 +0530

    Updates DB

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -4,6 +4,7 @@ function connectToDb() {
     mongoose.connect(process.env.DB_CONNECT)
     .then(() => console.log('Connected To DB'))
     .catch(err => console.log('Failed to connect to DB:', err));
+
 }
 
 module.exports = connectToDb;
